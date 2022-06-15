df3a783e73027c37af6f5aa68773c762b89ab313
Adri Garcia <Adrian.Vicente@gft.com>
2022-06-15 14:17:02 +0200

    SmokeTest running

--- src/test/java/com/example/SpringTest/WebLayerTest.java
+++ src/test/java/com/example/SpringTest/WebLayerTest.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.core.StringContains.containsString;
+
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @WebMvcTest
 public class WebLayerTest {
