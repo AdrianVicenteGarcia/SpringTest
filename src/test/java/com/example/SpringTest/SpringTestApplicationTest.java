255cd63fda0b3aade0f8d599655d18014cc50675
Adri Garcia <Adrian.Vicente@gft.com>
2022-06-15 16:22:03 +0200

    Adding all the tests

---
+++ src/test/java/com/example/SpringTest/SpringTestApplicationTest.java
@@ -0,0 +1,14 @@
+package com.example.SpringTest;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringTestApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
