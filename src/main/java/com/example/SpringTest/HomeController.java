03e0b82656d1897e7d70c5c1b86ff7227a6ed339
Adri Garcia <Adrian.Vicente@gft.com>
2022-06-16 09:07:16 +0200

    Adding all the tests

--- src/main/java/com/example/SpringTest/HomeController.java
+++ src/main/java/com/example/SpringTest/HomeController.java
@@ -1,15 +1,20 @@
 package com.example.SpringTest;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller //anotado como controlador
+@RestController //anotado como controlador
 public class HomeController {
 
     @RequestMapping("/") //donde quieres el mapeo
     public @ResponseBody String greeting() {
         return "Me encanta programar";
     }
-
+    @GetMapping("/add")
+    public Float add(
+        @RequestParam(value = "a", defaultValue = "0") Float a,
+        @RequestParam(value = "b", defaultValue = "0") Float b
+    ){
+        return 0f;
+    }
 }
