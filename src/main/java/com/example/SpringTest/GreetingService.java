45257bccdcb18c25a5c802bc703860d41edb7b84
Adri Garcia <Adrian.Vicente@gft.com>
2022-06-17 09:48:10 +0200

    PIT

--- src/main/java/com/example/SpringTest/GreetingService.java
+++ src/main/java/com/example/SpringTest/GreetingService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingService {
     public String greet() {
-        return "Hello, World";
+
+        return "Me encanta programar";
     }
 }
